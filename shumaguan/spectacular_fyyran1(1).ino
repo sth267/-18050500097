aef2627405f3c60ef4c4c5b081b5ae96fe17aa3d
sth267 <52457815+sth267@users.noreply.github.com>
2019-07-04 21:39:21 +0800

    数码管作业

---
+++ shumaguan/spectacular_fyyran1(1).ino
@@ -0,0 +1,63 @@
+void setup()
+{
+  pinMode(2, OUTPUT);
+  pinMode(3, OUTPUT);
+  pinMode(4, OUTPUT);
+  pinMode(5, OUTPUT);
+  pinMode(6, OUTPUT);
+  Serial.begin(9600);
+}
+int income=0;
+void loop()
+{
+  
+  digitalWrite(2, LOW);
+  delay(10);
+  
+  if (Serial.available()>0)
+  {
+  	income=Serial.read();
+    income=income-'0';
+    if (income&0x1)
+    {
+    	digitalWrite(3, HIGH);
+    }
+    else
+    {
+    	digitalWrite(3, LOW);
+    }
+    
+    if((income>>1)&0x1)
+    {
+    	digitalWrite(4, HIGH);
+    }
+    else
+    {
+    	digitalWrite(4, LOW);
+    }
+    
+    if((income>>2)&0x1)
+    {
+    	digitalWrite(5, HIGH);
+    }
+    else
+    {
+    	digitalWrite(5, LOW);
+    }
+    
+    
+    if((income>>3)&0x1)
+    {
+    	digitalWrite(6, HIGH);
+    }
+    else
+    {
+    	digitalWrite(6, LOW);
+    }
+  }
+  
+  delay(10);
+  digitalWrite(0,HIGH);
+  
+  
+}
