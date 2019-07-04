7bc1ce69678fdc1e75e961c61d5cc1306646583f
sth267 <52457815+sth267@users.noreply.github.com>
2019-07-04 15:15:28 +0800

    课上模拟小车作业

---
+++ THE car/daring_bruticus1.ino
@@ -0,0 +1,75 @@
+//f前进，b后退，l左转，r右转，s静止
+
+void setup()
+{
+  pinMode(5, OUTPUT);
+  pinMode(6,OUTPUT);
+  pinMode(9,OUTPUT);
+  pinMode(10,OUTPUT);
+  Serial.begin(9600);
+}
+
+int income = 0;
+void loop()
+{
+  if (Serial.available()> 0);
+  {
+    income=Serial.read();
+  }
+  switch (income)
+  {
+  	case 'f':
+    	forward();
+    	break;
+    case 'b':
+    	backward();
+    	break;
+    case 'r':
+    	right();
+    	break;
+    case 'l':
+    	left();
+    	break;
+    case 's':
+    	stop();
+    	break;
+    default:
+    	break;
+  }
+}
+
+void forward()
+{
+	digitalWrite(5,HIGH);
+  	digitalWrite(6,LOW);
+    digitalWrite(9,HIGH);
+    digitalWrite(10,LOW);
+}
+void backward()
+{
+	digitalWrite(6,HIGH);
+  	digitalWrite(5,LOW);
+    digitalWrite(10,HIGH);
+    digitalWrite(9,LOW);
+}
+void right()
+{
+	digitalWrite(6,HIGH);
+  	digitalWrite(5,LOW);
+    digitalWrite(9,HIGH);
+    digitalWrite(10,LOW);
+}
+void left()
+{
+	digitalWrite(5,HIGH);
+  	digitalWrite(6,LOW);
+    digitalWrite(10,HIGH);
+    digitalWrite(9,LOW);
+}
+void stop()
+{
+	digitalWrite(5,HIGH);
+  	digitalWrite(6,HIGH);
+    digitalWrite(9,HIGH);
+    digitalWrite(10,HIGH);
+}
